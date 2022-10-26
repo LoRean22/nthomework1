54d63c9317a026e0daaaec29b0e8e6f204c9c561
Dmitriy Loginov <brutalyso@gmail.com>
2022-10-26 16:20:33 +0300

    EDIT

--- FigureNumberOfSide/FigureNumberOfSide/FigureNumberOfSide.cpp
+++ FigureNumberOfSide/FigureNumberOfSide/FigureNumberOfSide.cpp
@@ -1,64 +1,62 @@
 ﻿#include <iostream>
 #include <string>
 
-class Figure 
+class Figure
 {
 protected:
 	int sides_count = 0;
 	std::string name = "";
 public:
-	Figure(int sides_count) 
+	Figure(int sides_count)
 	{
+		this->sides_count = sides_count;
+		this->sides_count = sides_count;
 		this->sides_count = sides_count;
 	}
 
-	virtual void getSidesCount() 
+	virtual void getSidesCount()
 	{
 		std::cout << name << ": " << sides_count << std::endl;
 	}
-	virtual void getName(std::string nameFigure) 
+	virtual void setName(std::string nameFigure)
 	{
 		name = nameFigure;
 	}
 };
 
-class Triangle : public Figure 
+class Triangle : public Figure
 {
 public:
-	Triangle(int sides_count) : Figure(0) 
-	{
-		this->sides_count = sides_count;
-	}
+	Triangle(int sides_count) : Figure(3) {}
 
-	void getSidesCount() override 
+	void getSidesCount() override
 	{
 		std::cout << name << ": " << sides_count << std::endl;
 	}
-	void getName(std::string nameFigure) override 
+
+	void setName(std::string nameFigure) override
 	{
 		name = nameFigure;
 	}
 };
 
-class Quadrangle : public Figure 
+class Quadrangle : public Figure
 {
 public:
-	Quadrangle(int sides_count) : Figure(0) 
-	{
-		this->sides_count = sides_count;
-	}
+	Quadrangle(int sides_count) : Figure(4) {}
 
-	void getSidesCount() override 
+	void getSidesCount() override
 	{
 		std::cout << name << ": " << sides_count << std::endl;
 	}
-	void getName(std::string nameFigure) override 
+
+	void setName(std::string nameFigure) override
 	{
 		name = nameFigure;
 	}
 };
 
-int main() 
+int main()
 {
 	setlocale(LC_ALL, "Russian");
 
@@ -69,16 +67,16 @@ int main()
 	std::cout << "Количество сторон:" << std::endl;
 
 	Figure fig(0);
-	fig.getName(namefigure);
+	fig.setName(namefigure);
 	fig.getSidesCount();
 
 	Triangle trian(3);
 	Figure* par_trian = &trian;
-	par_trian->getName(nameTriangle);
+	par_trian->setName(nameTriangle);
 	par_trian->getSidesCount();
 
 	Quadrangle quad(4);
 	Figure* par_quad = &quad;
-	par_quad->getName(nameQuadrangle);
+	par_quad->setName(nameQuadrangle);
 	par_quad->getSidesCount();
 }
